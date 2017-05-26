ddb043ed7c144bb2abf9b4db51a61f5ae18598a7
pateljay143 <pateljay143@gmail.com>
2017-05-25 23:27:38 -0700

    Table cell model

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by JAY PATEL on 5/25/17.
+//  Copyright © 2017 Jay. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+    func configCell(_ item: Item) {
+        title.text = item.title
+        price.text = "$\(item.price)"
+        details.text = item.details
+    }
+}
